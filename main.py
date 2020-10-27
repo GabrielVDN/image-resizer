47b22fddee69cfd8229a64ba1451b8e7ec8f4a0d
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-10-27 22:32:19 +0100

    Custon geometry

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class ImageResizer(tk.Tk):
 
         # Give the Widget a name.
         self.title("Image Resizer")
+        # Give the Widget a size.
+        self.geometry("400x200")
         # Center your Frame in the middle-top.
         self.columnconfigure(0, weight=1)
 
