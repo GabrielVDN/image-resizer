c7fcc636b25e64124ecef5b2e4dac8c4f7f88999
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-10-29 21:51:48 +0100

    frames

---
+++ frames/download_page.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class DownloadPage(ttk.Frame):
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        
+        self.controller = controller
+        # Center your Frame in the middle-top.
+        self.columnconfigure(0, weight=1)
+
+        # Add some labels.
+        laberl1 = ttk.Label(self, text="View Stock")
+        laberl1.grid()
