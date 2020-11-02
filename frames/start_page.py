6d24581bc7b5dad629db06814dcb26bebf4b0be3
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-11-02 22:32:20 +0100

    added an if for filename

--- frames/start_page.py
+++ frames/start_page.py
@@ -15,8 +15,9 @@ class StartPage(ttk.Frame):
         def UploadAction(event=None):
             filename = filedialog.askopenfilename(initialdir="\\Users\\gabri\\OneDrive\\Pictures\\SavedPictures", title="Select An Image", filetypes=(("jpeg files", "*.jpg"), ("gif files", "*.gif*"), ("png files", "*.png")))
 
-            image_label = ttk.Label(self, text=filename)
-            image_label.grid(row=3, column=0, padx=12, pady=12)
+            if filename:
+                image_label = ttk.Label(self, text=filename)
+                image_label.grid(row=3, column=0, padx=12, pady=12)
 
         # Add some labels.
         laberl1 = ttk.Label(self, text="Start Page")
