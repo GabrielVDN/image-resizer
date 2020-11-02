3df3e51ea58b1c2454773d2497c96887047ab16a
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-11-02 22:17:22 +0100

    import pil

--- frames/start_page.py
+++ frames/start_page.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from PIL import ImageTk, Image
 
 
 class StartPage(ttk.Frame):
@@ -11,11 +12,19 @@ class StartPage(ttk.Frame):
         # Center your Frame in the middle-top.
         self.columnconfigure(0, weight=1)
 
+        def UploadAction(event=None):
+            filename = filedialog.askopenfilename(initialdir="\\Users\\gabri\\OneDrive\\Pictures\\SavedPictures", title="Select An Image", filetypes=(("jpeg files", "*.jpg"), ("gif files", "*.gif*"), ("png files", "*.png")))
+            image_label = ttk.Label(self, text=filename)
+            image_label.grid(row=3, column=0, padx=12, pady=12)
+            my_image = ImageTk.PhotoImage(Image.open(filename))
+            my_image_label = ttk.Label(self, image=my_image)
+            my_image_label.grid(row=4, column=0, padx=12, pady=12)
+
         # Add some labels.
         laberl1 = ttk.Label(self, text="Start Page")
         laberl1.grid(row=0, column=0, padx=12, pady=12)
 
-        button = ttk.Button(self, text='Open', command=self.UploadAction)
+        button = ttk.Button(self, text='Open', command=UploadAction)
         button.grid(row=1, column=0, padx=12, pady=12, sticky="EW")
 
         download_page_button = ttk.Button(
@@ -24,8 +33,3 @@ class StartPage(ttk.Frame):
             command=lambda: controller.show_frame("DownloadPage"),
         )
         download_page_button.grid(row=2, column=0, padx=12, pady=12, sticky="EW")
-
-
-    def UploadAction(event=None):
-        filename = filedialog.askopenfilename(initialdir="/", title="Select An Image", filetypes=(("jpeg files", "*.jpg"), ("gif files", "*.gif*"), ("png files", "*.png")))
-        print('Selected:', filename)
