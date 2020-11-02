f7a18d29470d8fb227d56eca4061ec0994c2443b
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-11-02 21:57:49 +0100

    change column and row

--- frames/start_page.py
+++ frames/start_page.py
@@ -12,11 +12,11 @@ class StartPage(ttk.Frame):
 
         # Add some labels.
         laberl1 = ttk.Label(self, text="Start Page")
-        laberl1.grid()
+        laberl1.grid(row=0, column=0)
 
         download_page_button = ttk.Button(
             self,
             text="Download Page",
             command=lambda: controller.show_frame("DownloadPage"),
         )
-        download_page_button.grid(row=1, column=0, padx=12, pady=12, sticky="EW")
+        download_page_button.grid(row=2, column=0, padx=12, pady=12, sticky="NE")
