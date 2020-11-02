ec2085c5f591d0f2c82f571bd213c561604a74bc
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-11-02 22:12:34 +0100

    openfilename

--- frames/start_page.py
+++ frames/start_page.py
@@ -15,17 +15,17 @@ class StartPage(ttk.Frame):
         laberl1 = ttk.Label(self, text="Start Page")
         laberl1.grid(row=0, column=0, padx=12, pady=12)
 
-        button = tk.Button(root, text='Open', command=self.UploadAction)
-        button.grid(row=1, column=0, padx=12, pady=12)
+        button = ttk.Button(self, text='Open', command=self.UploadAction)
+        button.grid(row=1, column=0, padx=12, pady=12, sticky="EW")
 
         download_page_button = ttk.Button(
             self,
             text="Download Page",
             command=lambda: controller.show_frame("DownloadPage"),
         )
-        download_page_button.grid(row=2, column=0, padx=12, pady=12, sticky="NE")
+        download_page_button.grid(row=2, column=0, padx=12, pady=12, sticky="EW")
 
 
     def UploadAction(event=None):
-        filename = filedialog.askopenfilename()
+        filename = filedialog.askopenfilename(initialdir="/", title="Select An Image", filetypes=(("jpeg files", "*.jpg"), ("gif files", "*.gif*"), ("png files", "*.png")))
         print('Selected:', filename)
